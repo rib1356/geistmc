8936e28e6c806dcfbd71d352456748e8c8166ef5
Rob Brown <rob@latticetraining.com>
2023-07-25 00:09:23 +0100

    Focus artist images to components and add on hover effect

---
+++ src/components/ArtistComponents/FocusArtistImageComponent.vue
@@ -0,0 +1,78 @@
+<template>
+    <div :class="hover ? 'focus-image-hover' : 'focus-image'" @mouseover="hover = true" @mouseleave="hover = false" @click="doSomething()">
+        <img :src="(`${this.baseURL}images/artists/focusArtists/${focusArtistImagePath}`)" :alt="(`${focusArtistName}-focus-image`)" :class="hover ? 'artist-image-hover' : 'artist-image'"/>
+        <span v-if="hover" class="focus-image-text">{{ focusArtistName }}</span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'FocusArtistImageComponent',
+    data () {
+        return {
+			baseURL: import.meta.env.PROD === true ? '' : '/', //Used as base URL for images as annoyingly local images need /
+            hover: false,
+        }
+    },
+    props: {
+        focusArtistImagePath: {
+            type: String
+        },
+        focusArtistName: {
+            type: String
+        },
+    },
+    methods: {
+        doSomething() {
+            alert("Hey you clicked " + this.focusArtistName + " this will take you to their page");
+        }
+    },
+}
+</script>
+
+<style scoped>
+    .focus-image {
+        width: 16vw;
+        height: 32vh;
+        border: 1px solid #DFDFDF;
+        margin-bottom: 1.7rem;
+        position: relative;
+        text-align: center;
+        color: white;
+    }
+
+    .focus-image-hover {
+        background: black;
+        width: 16vw;
+        height: 32vh;
+        border: 1px solid #DFDFDF;
+        margin-bottom: 1.7rem;
+        position: relative;
+        text-align: center;
+        color: white;
+    }
+    .artist-image {
+        object-fit: cover;
+        height: 100%;
+        width: 100%;
+    }
+
+    .focus-image-text {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        font-family: 'Montserrat-Bold', sans-serif;
+        font-size: 20px;
+    }
+
+    .artist-image-hover {
+        opacity: 20%;
+        object-fit: cover;
+        height: 100%;
+        width: 100%;
+        transition: 0.25s ease-out;
+    }
+
+</style>
+  
--- src/components/ArtistComponents/FocusArtistsComponent.vue
+++ src/components/ArtistComponents/FocusArtistsComponent.vue
@@ -1,40 +1,38 @@
+<script setup>
+    import FocusArtistImageComponent from './FocusArtistImageComponent.vue';
+</script>
 <template>
     <div class="focus-artists-div">
         <h2 class="focus-artists-text">Focus Artists</h2>
         <div class="focus-image-flex">
-            <div class="focus-image">
-                <img :src="(`${this.baseURL}images/artists/focusArtists/1ArturoDiModica.jpg`)" alt="arturo-di-modica-focus-artist" class="artist-image"/>
-            </div>
-            <div class="focus-image">
-                <img :src="(`${this.baseURL}images/artists/focusArtists/2DavidHockney.jpg`)" alt="david-hockney-focus-artist" class="artist-image"/>
-            </div>
-            <div class="focus-image">
-                <img :src="(`${this.baseURL}images/artists/focusArtists/3AndyWarhol.jpg`)" alt="andy-warhol-focus-artist" class="artist-image"/>
-            </div>
-            <div class="focus-image">
-                <img :src="(`${this.baseURL}images/artists/focusArtists/4Banksy.jpg`)" alt="banksy-focus-artist" class="artist-image"/>
-            </div>
-            <div class="focus-image">
-                <img :src="(`${this.baseURL}images/artists/focusArtists/5FrancisBacon.jpg`)" alt="francis-bacon-focus-artist" class="artist-image"/>
-            </div>
-            <div class="focus-image">
-                <img :src="(`${this.baseURL}images/artists/focusArtists/6SalvadorDali.jpg`)" alt="salvador-dali-focus-artist" class="artist-image"/>
-            </div>
-            <div class="focus-image">
-                <img :src="(`${this.baseURL}images/artists/focusArtists/7YayoiKusama.jpg`)" alt="yayoi-kusama-focus-artist" class="artist-image"/>
-            </div>
-            <div class="focus-image">
-                <img :src="(`${this.baseURL}images/artists/focusArtists/8RoyLichtenstein.jpg`)" alt="roy-lichtenstein-focus-artist" class="artist-image"/>
-            </div>
+            <FocusArtistImageComponent 
+                v-for="artist in focusArtists" 
+                :key="artist.artistName"
+                :focusArtistImagePath="artist.imagePath" 
+                :focusArtistName="artist.artistName">
+            </FocusArtistImageComponent>
         </div>
     </div> 
 </template>
 <script>
 export default {
     name: 'FocusArtistsComponent',
+    components: {
+        FocusArtistImageComponent
+    },
     data () {
         return {
 			baseURL: import.meta.env.PROD === true ? '' : '/', //Used as base URL for images as annoyingly local images need /
+            focusArtists: [
+                { imagePath: "1ArturoDiModica.jpg", artistName: "Arturo Di Modica" },
+                { imagePath: "2DavidHockney.jpg", artistName: "David Hockney" },
+                { imagePath: "3AndyWarhol.jpg", artistName: "Andy Warhol" },
+                { imagePath: "4Banksy.jpg", artistName: "Banksy" },
+                { imagePath: "5FrancisBacon.jpg", artistName: "Francis Bacon" },
+                { imagePath: "6SalvadorDali.jpg", artistName: "Salvador Dali" },
+                { imagePath: "7YayoiKusama.jpg", artistName: "Yayoi Kusama" },
+                { imagePath: "8RoyLichtenstein.jpg", artistName: "Roy Lichtenstein" }
+            ]
         }
     },
     mounted () {
@@ -58,19 +56,5 @@ export default {
         flex-direction: row;
         justify-content: space-between;
     }
-
-    .focus-image {
-        width: 16vw;
-        height: 32vh;
-        border: 1px solid #DFDFDF;
-        margin-bottom: 1.7rem;
-    }
-
-    .artist-image {
-        object-fit: cover;
-        height: 100%;
-        width: 100%;
-    }
-
 </style>
   
